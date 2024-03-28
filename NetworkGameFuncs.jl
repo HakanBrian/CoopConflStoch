fb51e58e40970d899afbc8ac84896077347da19f
HakanBrian <chetas.chavda@outlook.com>
2024-03-27 23:12:37 -0400

    add onto fitness
    removed useless parts
    added new functions to be created
    added comments

--- NetworkGameFuncs.jl
+++ NetworkGameFuncs.jl
@@ -1,5 +1,5 @@
-using LinearAlgebra, Random, Distributions, ArgParse, StatsBase, DataFrames
-
+using LinearAlgebra, Random, Distributions, StatsBase, DataFrames
+s
 ####################################
 # Network Game Functions
 ####################################
@@ -10,40 +10,67 @@ include("NetworkGameStructs.jl")
 # Population Simulation Funcs #
 ###############################
 
-function update_population!(pop::population)
-
-end
+    # create a blank starting population
+    # format output
 
 function population_construction(parameters::simulation_parameters)
     ## constructs a population array when supplied with parameters
-    return population(parameters, shuffle(1:parameters.N), zeros(Float64, parameters.N),zeros(Float64, parameters.N),zeros(Float64, parameters.N),zeros(Float64, parameters.N),zeros(Float64, parameters.N),0)
+    return population(parameters, zeros(Float64, parameters.N),zeros(Float64, parameters.N),zeros(Float64, parameters.N),zeros(Float64, parameters.N),zeros(Float64, parameters.N),0)
 end
+
+function output(t::Int64, pop::population, outputs::DataFrame)
 
 ##################
 # Pairwise fitness
 ##################
 
-function social_interactions!(pop::population)
-    # match individuals randomly in pairs
-    if pop.parameters.N % 2 == 0
-        pop.shuffled_indices = shuffle(1:pop.parameters.N)
-    else
-        # if population size is odd, last individual plays themself
-        shfld = shuffle(1:pop.parameters.N)
-        pop.shuffled_indices = [shfld; shfld[end]]
-    end
+    # pair individuals with the possibiliy of pairing more than once
+    # everyone has the same chance of picking a partner / getting picked
+    # at the end of the day everyone is picked roughly an equal number of times aka bootstrap
+    # calculate payoff, keep a running average of payoff for each individual
+    # after each session of interaction the running average becomes the individual's payoff
 
-    return pop.shuffled_indices
+function benefit(pop::population)
+
+end
+
+function cost(pop::population)
+
+end
+
+function payoff(pop::population)
+
+end
+
+function social_interactions(pop::population)
+
 end
 
 ##################
 # Reproduction function
 ##################
 
-function reproduce!(pop::population)
-    pop.mean_w = mean(pop.payoffs)
+    # offspring inherit the payoff or traits of the parents?
+    # only need one parent
+    # number of individuals in population remains the same
+
+function reproduce(pop::population)
     
 end
+
+##################
+#  Mutation Function 
+##################
+
+    # mutate?
+
+function mutate(pop::population)
+
+end
+
+#######################
+# Simulation Function #
+#######################
 
 function simulation(pop::population)
 
@@ -59,18 +86,20 @@ function simulation(pop::population)
       
     for t in 1:pop.parameters.tmax
     
-        # update population struct 
-        update_population!(pop)
-    
         # execute social interactions and calculate payoffs
         social_interactions!(pop)
     
-        # reproduction function to produce and save t+1 population array
-        reproduce!(pop)
-    
+        # reproduction function to produce new population
+        reproduce(pop)
+
+        # mutation function  iterates over population and mutates at chance probability μ
+        if pop.parameters.u > 0
+            mutate(pop)
+        end
+
         # per-timestep counters, outputs going to disk
         if t % pop.parameters.output_save_tick == 0
-            output!(t, copy(pop), outputs)
+            output(t, copy(pop), outputs)
         end
     
     end
--- NetworkGameStructs.jl
+++ NetworkGameStructs.jl
@@ -1,10 +1,9 @@
-using StaticArrays
-
 mutable struct simulation_parameters
     #popgen params
     tmax::Int64
     nreps::Int64
     N::Int64
+    u::Float64
     #game params
     action0::Float64
     a0::Float64
@@ -12,25 +11,12 @@ mutable struct simulation_parameters
     T0::Float64
 end
 
-mutable struct network
-    genotype_id::Int64
-    Wm::SMatrix
-    Wb::SVector
-    InitialOffer::Float64
-    CurrentOffer::Float64
-end
-
 mutable struct population
     parameters::simulation_parameters
-    shuffled_indices::Vector{Int64}
     action::Vector{Float64}
     a::Vector{Float64}
     p::Vector{Float64}
     T::Vector{Float64}
     payoffs::Vector{Float64}
     mean_w::Float64
-end
-
-mutable struct pop_parameters
-    Tuples::Vector{Tuple{Float64,Float64,Float64,Float64}}
 end
