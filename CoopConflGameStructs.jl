7242d860bf8f51c8503ee5d5fb81c3ee2cf0125f
HakanBrian <chetas.chavda@outlook.com>
2024-10-09 21:52:21 -0400

    Revert "fix: virtual individual handling"
    This reverts commit 02a89c3959a20f0b6ad79f7d4741692e95253d7c.

--- CoopConflGameStructs.jl
+++ CoopConflGameStructs.jl
@@ -12,9 +12,9 @@ mutable struct SimulationParameters
     gmax::Int64  # maximum number of generations
     tmax::Float64  # maximum length of timespan for ODE
     population_size::Int64
+    group_size::Int64
     synergy::Float64
     relatedness::Float64
-    inflation_factor::Int64
     fitness_scaling_factor_a::Float64
     fitness_scaling_factor_b::Float64
     mutation_rate::Float64
@@ -32,9 +32,9 @@ mutable struct SimulationParameters
         gmax::Int64=100000,
         tmax::Float64=5.0,
         population_size::Int64=50,
+        group_size::Int64 = 10,
         synergy::Float64=0.0,
         relatedness::Float64=0.5,
-        inflation_factor::Int64=0,
         fitness_scaling_factor_a::Float64=0.004,
         fitness_scaling_factor_b::Float64=10.0,
         mutation_rate::Float64=0.05,
@@ -42,7 +42,7 @@ mutable struct SimulationParameters
         trait_variance::Float64=0.0,
         output_save_tick::Int64=10
     )
-        new(action0, a0, p0, T0, gmax, tmax, population_size, synergy, relatedness, inflation_factor, fitness_scaling_factor_a, fitness_scaling_factor_b, mutation_rate, mutation_variance, trait_variance, output_save_tick)
+        new(action0, a0, p0, T0, gmax, tmax, population_size, group_size, synergy, relatedness, fitness_scaling_factor_a, fitness_scaling_factor_b, mutation_rate, mutation_variance, trait_variance, output_save_tick)
     end
 end
 
@@ -55,9 +55,9 @@ function Base.copy(parameters::SimulationParameters)
         gmax=getfield(parameters, :gmax),
         tmax=getfield(parameters, :tmax),
         population_size=getfield(parameters, :population_size),
+        group_size=getfield(parameters, :group_size),
         synergy=getfield(parameters, :synergy),
         relatedness=getfield(parameters, :relatedness),
-        inflation_factor=getfield(parameters, :inflation_factor),
         fitness_scaling_factor_a=getfield(parameters, :fitness_scaling_factor_a),
         fitness_scaling_factor_b=getfield(parameters, :fitness_scaling_factor_b),
         mutation_rate=getfield(parameters, :mutation_rate),
@@ -75,9 +75,9 @@ function Base.copy!(old_params::SimulationParameters, new_params::SimulationPara
     setfield!(old_params, :gmax, getfield(new_params, :gmax))
     setfield!(old_params, :tmax, getfield(new_params, :tmax))
     setfield!(old_params, :population_size, getfield(new_params, :population_size))
+    setfield!(old_params, :group_size, getfield(new_params, :group_size))
     setfield!(old_params, :synergy, getfield(new_params, :synergy))
     setfield!(old_params, :relatedness, getfield(new_params, :relatedness))
-    setfield!(old_params, :inflation_factor, getfield(new_params, :inflation_factor))
     setfield!(old_params, :fitness_scaling_factor_a, getfield(new_params, :fitness_scaling_factor_a))
     setfield!(old_params, :fitness_scaling_factor_b, getfield(new_params, :fitness_scaling_factor_b))
     setfield!(old_params, :mutation_rate, getfield(new_params, :mutation_rate))
