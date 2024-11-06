590821d8062be6a1e3b370fe8989bff1800142bb
HakanBrian <chetas.chavda@outlook.com>
2024-11-06 14:18:57 -0500

    fix: redef of Main.simulation

--- SLURM/CoopConflGameSLURM.jl
+++ SLURM/CoopConflGameSLURM.jl
@@ -2,8 +2,8 @@ using Distributed
 addprocs(4);
 
 using BenchmarkTools
-@everywhere include(joinpath(@__DIR__, "../CoopConflGameFuncs.jl"))
-#=
+@everywhere include("../CoopConflGamePlots.jl")
+
 relatedness_values = collect(range(0, 1, step=0.01));
 
 parameter_sweep = [
@@ -19,16 +19,5 @@ parameter_sweep = [
 ]
 
 simulation_sweep = simulation_replicate(parameter_sweep, 40);
-=#
-param=SimulationParameters(action0=0.1f0,
-norm0=2.0f0,
-ext_pun0=0.1f0,
-int_pun_ext0=0.0f0,
-int_pun_self0=0.0f0,
-population_size=50,
-group_size=10,
-relatedness=r_values)
 
-simulation = simulation_replicate(param, 5);
-
-save_simulation(simulation, joinpath(@__DIR__, "simulation.csv"))
+save_simulation(my_simulation, joinpath(@__DIR__, "simulation_sweep.csv"))
