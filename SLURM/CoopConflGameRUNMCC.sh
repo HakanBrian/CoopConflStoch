6560aa27481ea93af57decc6fbd3a6d2902eb760
HakanBrian <chetas.chavda@outlook.com>
2024-11-06 14:48:36 -0500

    feat: add mailing
    send email when start and stop job

--- SLURM/CoopConflGameRUNMCC.sh
+++ SLURM/CoopConflGameRUNMCC.sh
@@ -6,8 +6,9 @@
 #SBATCH -e slurm-%j.err             # Error file for this job.
 #SBATCH -o slurm-%j.out             # Output file for this job.
 #SBATCH -A <your project account>     # Project allocation account name (REQUIRED)
+#SBATCH --mail-type ALL             # Send email when job starts/ends
  
-module purge                  # Unload other software modules
+module purge                      # Unload other software modules
 
 # Execute the Julia script
 julia CoopConflGameSLURM.jl
