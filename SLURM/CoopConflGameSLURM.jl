f603d706e1a19911f7e7147d536e10f0eac5d0b3
HakanBrian <chetas.chavda@outlook.com>
2024-11-12 19:57:56 -0500

    feat: add heatmap data generation

--- SLURM/CoopConflGameSLURM.jl
+++ SLURM/CoopConflGameSLURM.jl
@@ -4,9 +4,10 @@ addprocs(4);
 using BenchmarkTools
 @everywhere include("../CoopConflGamePlots.jl")
 
-relatedness_values = collect(range(0, 1, step=0.01));
 
-parameter_sweep = [
+r1_values = collect(range(0, 1.0, step=0.01));
+
+parameter_sweep_r = [
     SimulationParameters(action0=0.1f0,
                          norm0=2.0f0,
                          ext_pun0=0.1f0,
@@ -15,9 +16,31 @@ parameter_sweep = [
                          population_size=50,
                          group_size=10,
                          relatedness=r_values)
-    for r_values in relatedness_values
+    for r_values in r1_values
 ]
 
-simulation_sweep = simulation_replicate(parameter_sweep, 40);
+simulation_sweep_r = simulation_replicate(parameter_sweep_r, 40);
 
-save_simulation(simulation_sweep, joinpath(@__DIR__, "simulation_sweep.csv"))
+save_simulation(simulation_sweep_r, joinpath(@__DIR__, "simulation_sweep_r1.csv"))
+
+
+r05_values = collect(range(0, 0.5, step=0.01));
+ep05_values = collect(range(0, 0.5, step=0.01));
+
+parameter_sweep_rep = [
+    SimulationParameters(action0=0.1f0,
+                         norm0=2.0f0,
+                         ext_pun0=ep_values,
+                         int_pun_ext0=0.0f0,
+                         int_pun_self0=0.0f0,
+                         population_size=50,
+                         group_size=10,
+                         relatedness=r_values,
+                         ext_pun_mutation_enabled=false)
+    for r_values in r05_values
+    for ep_values in ep05_values
+]
+
+simulation_sweep_rep = simulation_replicate(parameter_sweep_rep, 40);
+
+save_simulation(simulation_sweep_rep, joinpath(@__DIR__, "simulation_sweep_rep1.csv"))
