84b3dc6b2b15afe87befb9cef8ffe73829136b29
HakanBrian <chetas.chavda@outlook.com>
2024-11-20 22:50:09 -0500

    chore: add option for nodes

--- SLURM/CoopConflGameRUNMCC.sh
+++ SLURM/CoopConflGameRUNMCC.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --time=06:00:00             # Time limit for the job (REQUIRED).
 #SBATCH --job-name=CoopConflGame    # Job name
+#SBATCH --nodes=1                   # Number of nodes to allocate. Same as SBATCH -N (Don't use this option for mpi jobs)
 #SBATCH --exclusive                 # Allocate all cores in node.
 #SBATCH --partition=normal          # Partition/queue to run the job in. (REQUIRED)
 #SBATCH -e slurm-%j.err             # Error file for this job.
