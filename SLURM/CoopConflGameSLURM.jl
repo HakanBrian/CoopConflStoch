35a9d43b6e8821b16fc33ef16511a9d64cb11052
HakanBrian <140771010+HakanBrian@users.noreply.github.com>
2024-12-29 09:43:30 -0600

    feat: predefine parameters
    one param used by all runs to stay consistent.

--- SLURM/CoopConflGameSLURM.jl
+++ SLURM/CoopConflGameSLURM.jl
@@ -7,8 +7,13 @@ addprocs(128);
 # Run Simulation
 ###############################
 
-run_sim_r("simulation_sweep_r_stats.csv")
+base_params = SimulationParameters(action0=0.1f0,
+                                    norm0=2.0f0,
+                                    ext_pun0=0.1f0,
+                                    population_size=500)
 
-run_sim_rep("simulation_sweep_rep_stats.csv")
+run_sim_r(base_params, "simulation_sweep_r_stats.csv")
 
-run_sim_rgs("simulation_sweep_rgs_stats.csv")
+run_sim_rep(base_params, "simulation_sweep_rep_stats.csv")
+
+run_sim_rgs(base_params, "simulation_sweep_rgs_stats.csv")
