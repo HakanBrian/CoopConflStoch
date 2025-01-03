1c49e624fba71391b20c8d23779a749bc6ffd58f
HakanBrian <140771010+HakanBrian@users.noreply.github.com>
2025-01-03 03:18:02 -0600

    style: change naming convention of data

--- SLURM/CoopConflGameSLURM.jl
+++ SLURM/CoopConflGameSLURM.jl
@@ -19,8 +19,8 @@ base_params = SimulationParameters(action0=0.1f0,
                                     ext_pun0=0.1f0,
                                     population_size=500)
 
-run_sim_r(base_params, "simulation_sweep_r_stats.csv")
+run_sim_r(base_params, "r1.csv")
 
-run_sim_rep(base_params, "simulation_sweep_rep_stats.csv")
+run_sim_rep(base_params, "rep1.csv")
 
-run_sim_rgs(base_params, "simulation_sweep_rgs_stats.csv")
+run_sim_rgs(base_params, "rgs1.csv")
