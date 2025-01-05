6cc8e21d42491bb0385c7c15745f555e7a27c829
HakanBrian <140771010+HakanBrian@users.noreply.github.com>
2025-01-04 21:45:45 -0600

    refactor: base_params more explicit

--- SLURM/CoopConflGameSLURM.jl
+++ SLURM/CoopConflGameSLURM.jl
@@ -17,7 +17,15 @@ include("CoopConflGameSLURMHelper.jl");
 base_params = SimulationParameters(action0=0.1f0,
                                     norm0=2.0f0,
                                     ext_pun0=0.1f0,
-                                    population_size=500)
+                                    int_pun_ext0=0.0f0,
+                                    int_pun_self0=0.0f0,
+                                    generations=100000,
+                                    population_size=500,
+                                    group_size=10,
+                                    ext_pun_mutation_enabled=true,
+                                    int_pun_ext_mutation_enabled=true,
+                                    int_pun_self_mutation_enabled=true,
+                                    output_save_tick=10)
 
 run_sim_r(base_params, "r1.csv")
 
