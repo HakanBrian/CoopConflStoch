16cf5c478ae32e4fb5aaaa409dbf56e15fa04c19
HakanBrian <chetas.chavda@outlook.com>
2025-03-12 20:17:39 -0400

    fix: rip script

--- scripts/ripDiffGS/generate_ripDiffGS.jl
+++ scripts/ripDiffGS/generate_ripDiffGS.jl
@@ -55,5 +55,4 @@ run_simulation(
         :int_pun_ext0 => collect(range(0.0f0, 1.0f0, step = 0.05f0)),
         :group_size => [5, 50, 500],
     ),
-    linked_params = Dict(:int_pun_self0 => :int_pun_ext0),
 )
