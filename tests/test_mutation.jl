a770bd4b86447c52afc336ca35655290f9672ebf
HakanBrian <140771010+HakanBrian@users.noreply.github.com>
2025-07-01 15:11:21 -0500

    chore: add test for mean shifts

--- tests/test_mutation.jl
+++ tests/test_mutation.jl
@@ -1,4 +1,4 @@
-using BenchmarkTools, Revise
+using BenchmarkTools, Revise, Distributions
 
 push!(LOAD_PATH, abspath("src"));
 using MainSimulation
@@ -20,3 +20,13 @@ MainSimulation.mutate!(
     Simulations.truncation_bounds(population.parameters.mutation_variance, 0.99),
 )
 println(population)
+
+# Truncated distribtion has mean shift
+mutation_dist = Normal(0, 0.005)
+mean(mutation_dist)
+
+truncate_bounds = MainSimulation.Populations.truncation_bounds(0.005, 0.99)
+
+test_dist = truncated(mutation_dist, 0, truncate_bounds[2])
+mean(test_dist)
+std(test_dist)
