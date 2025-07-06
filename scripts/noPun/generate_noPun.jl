72466de96e703c3a96c6d83aac98e6b52ececd28
HakanBrian <140771010+HakanBrian@users.noreply.github.com>
2025-07-06 16:42:12 -0500

    update: include rgs scaling changes to noPun

--- scripts/noPun/generate_noPun.jl
+++ scripts/noPun/generate_noPun.jl
@@ -67,16 +67,27 @@ run_simulation(
     ),
 )
 
-# rgs
+# rgs linear
 run_simulation(
     base_params,
     num_replicates = 20,
     save_file = true,
-    filepath = "data/noPun/noPun_rgs",
+    filepath = "data/noPun/noPun_rgs_linear",
     sweep_vars = Dict{Symbol,Vector{<:Real}}(
-        :relatedness => collect(range(0, 1.0, step = 0.1)),
-        :group_size =>
-            [collect(range(5, 50, step = 5))..., collect(range(100, 500, step = 50))...],
+        :relatedness => collect(range(0, 1.0, step = 0.05)),
+        :group_size => collect(range(50, 500, step = 50)),
+    ),
+)
+
+# rgs log
+run_simulation(
+    base_params,
+    num_replicates = 20,
+    save_file = true,
+    filepath = "data/noPun/noPun_rgs_log",
+    sweep_vars = Dict{Symbol,Vector{<:Real}}(
+        :relatedness => collect(range(0, 1.0, step = 0.05)),
+        :group_size => round.(Int, exp.(range(log(5), log(500), length=10))),
     ),
 )
 
