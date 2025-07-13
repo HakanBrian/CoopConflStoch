dac3ec5b3348deacd3ef8ca966dd8ade7a0b7995
HakanBrian <140771010+HakanBrian@users.noreply.github.com>
2025-07-13 12:03:57 -0500

    update: noPun for repDiffGS

--- scripts/noPun/generate_noPun.jl
+++ scripts/noPun/generate_noPun.jl
@@ -111,7 +111,7 @@ run_simulation(
     sweep_vars = Dict{Symbol,Vector{<:Real}}(
         :relatedness => collect(range(0, 1.0, step = 0.05)),
         :ext_pun0 => zeros(Float32, 21),
-        :group_size => [5, 50, 500],
+        :group_size => [10, 50, 500],
     ),
 )
 
